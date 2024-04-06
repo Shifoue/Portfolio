d0199167a15e03a9dafdf36ea85e78260c4397b1
BRAND Romain <brandromain@yahoo.fr>
2024-04-06 16:11:05 +0200

    Upgraded Decoder model by adding feedforward conv block to create a better noise

--- GenAI_API/service/backend/ressources/AI_network/model/model_definitions/model_components/decoder/decoder.py
+++ GenAI_API/service/backend/ressources/AI_network/model/model_definitions/model_components/decoder/decoder.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 class BlockConv2D(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(BlockConv2D, self).__init__()
+        self.in_features = in_channels
+        self.out_features = out_channels
+
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
             nn.BatchNorm2d(out_channels),
@@ -16,13 +19,14 @@ class BlockConv2D(nn.Module):
     def forward(self, x):
       return self.double_conv(x)
 
-# class BlockUpsamble2D(nn.Module):
-
 class BlockConv2D_Upsample2D(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, conv_in_channels, conv_out_channels, up_in_channels, up_out_channels):
         super(BlockConv2D_Upsample2D, self).__init__()
-        self.conv = BlockConv2D(in_channels, in_channels)
-        self.upsample = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.in_features = conv_in_channels
+        self.out_features = up_out_channels
+
+        self.conv = BlockConv2D(conv_in_channels, conv_out_channels)
+        self.upsample = nn.ConvTranspose2d(up_in_channels, up_out_channels, kernel_size=2, stride=2)
 
     def forward(self, x):
         x = self.conv(x)
@@ -32,29 +36,19 @@ class BlockConv2D_Upsample2D(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_dim, input_resize_dim):
+    def __init__(self, input_dim, input_resize_dim, in_features, out_features=1):
         super(Decoder, self).__init__()
         self.input_linear = nn.Linear(input_dim, input_resize_dim) #nn.Conv1d(input_dim, input_resize_dim, 1)
-        self.block1 = BlockConv2D_Upsample2D(512, 256)
 
-        self.block2 = BlockConv2D_Upsample2D(256, 64)
+        self.block1 = BlockConv2D_Upsample2D(in_features, in_features, in_features, in_features)
+        self.block2 = BlockConv2D_Upsample2D(self.block1.out_features, self.block1.out_features, self.block1.out_features, self.block1.out_features // 2)
+        self.block3 = BlockConv2D_Upsample2D(self.block2.out_features, self.block2.out_features, self.block2.out_features, self.block2.out_features // 2)
+        self.block4 = BlockConv2D_Upsample2D(self.block3.out_features, self.block3.out_features, self.block3.out_features, self.block3.out_features // 2)
+        self.block5 = BlockConv2D_Upsample2D(self.block4.out_features, self.block4.out_features, self.block4.out_features, self.block4.out_features // 2)
 
-        #self.block3 = BlockConv2D_Upsample2D(128, 64)
-
-        self.block4 = BlockConv2D_Upsample2D(64, 16)
-
-        #self.block5 = BlockConv2D_Upsample2D(32, 16)
-
-        self.block6 = BlockConv2D_Upsample2D(16, 4)
-
-        #self.block7 = BlockConv2D_Upsample2D(8, 4)
-
-        #self.block8 = BlockConv2D_Upsample2D(4, 2)
-
-        self.block9 = BlockConv2D_Upsample2D(4, 1)
+        self.output_layer = nn.Conv2d(self.block5.out_features, out_features, kernel_size=1)
 
     def forward(self, x):
-
         #input shape : batch size, 768, 512
         x = self.input_linear(x)
         #shape : batch size, 1024, 512
@@ -62,12 +56,10 @@ class Decoder(nn.Module):
 
         x = self.block1(x)
         x = self.block2(x)
-        #x = self.block3(x)
+        x = self.block3(x)
         x = self.block4(x)
-        #x = self.block5(x)
-        x = self.block6(x)
-        #x = self.block7(x)
-        #x = self.block8(x)
-        x = self.block9(x)
+        x = self.block5(x)
+
+        x = self.output_layer(x)
 
         return x
